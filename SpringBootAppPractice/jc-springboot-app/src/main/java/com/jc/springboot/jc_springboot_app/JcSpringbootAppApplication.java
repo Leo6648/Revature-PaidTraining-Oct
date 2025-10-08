4af3d87ca2937caca93daffd641584d72b7a7db8
Leo6648 <jchen6648@gmail.com>
2025-10-08 10:48:03 -0500

    Add the basic setup for SpringBoot App

---
+++ SpringBootAppPractice/jc-springboot-app/src/main/java/com/jc/springboot/jc_springboot_app/JcSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.jc.springboot.jc_springboot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JcSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ SpringBootAppPractice/jc-springboot-app/src/test/java/com/jc/springboot/jc_springboot_app/JcSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.springboot.jc_springboot_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
