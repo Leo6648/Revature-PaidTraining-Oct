9bed26fd81bf93409da5fe5a3b9bae436a62ca9c
Leo6648 <jchen6648@gmail.com>
2025-10-08 21:10:42 -0500

    Using application.properties file

--- SpringBootAppPractice/jc-springboot-app/src/main/java/com/jc/springboot/jc_springboot_app/JcSpringbootAppApplication.java
+++ SpringBootAppPractice/jc-springboot-app/src/main/java/com/jc/springboot/jc_springboot_app/JcSpringbootAppApplication.java
@@ -2,8 +2,10 @@ package com.jc.springboot.jc_springboot_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class JcSpringbootAppApplication {
 
 	public static void main(String[] args) {
