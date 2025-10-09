64f01a09b0f18791c7baa6bf104b9efc83498bbc
Leo6648 <jchen6648@gmail.com>
2025-10-09 13:32:09 -0500

    Create SpringBootController Project - JC

---
+++ SBControlPrac/sb-controll-prac-jc/src/main/java/com/sb/controller/jc/sb_controll_prac_jc/SbControllPracJcApplication.java
@@ -0,0 +1,13 @@
+package com.sb.controller.jc.sb_controll_prac_jc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbControllPracJcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbControllPracJcApplication.class, args);
+	}
+
+}
---
+++ SBControlPrac/sb-controll-prac-jc/src/test/java/com/sb/controller/jc/sb_controll_prac_jc/SbControllPracJcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.controller.jc.sb_controll_prac_jc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbControllPracJcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
