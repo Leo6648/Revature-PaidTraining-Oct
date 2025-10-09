20477caca54fe218729bb1bdd0c43b0ba7dade46
Leo6648 <jchen6648@gmail.com>
2025-10-08 21:41:17 -0500

    Add in the yml file

--- SpringBootAppPractice/jc-springboot-app/src/main/java/com/jc/springboot/jc_springboot_app/JcSpringbootAppApplication.java
+++ SpringBootAppPractice/jc-springboot-app/src/main/java/com/jc/springboot/jc_springboot_app/JcSpringbootAppApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
+/*By default, Spring Boot loads both application.yml and application.properties from src/main/resources.
+If a property is defined in both, application.properties takes precedence.*/
 public class JcSpringbootAppApplication {
 
 	public static void main(String[] args) {
