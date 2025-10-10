086d6b27882d6d0467f0d1a425da3a7b2c59b663
Leo6648 <jchen6648@gmail.com>
2025-10-10 10:47:14 -0500

    Set up the files for SBJPA practice - JC

---
+++ SJPA_Practice/jpa-demo-jc/src/main/java/com/sb/jpa/jpa_demo_jc/JpaDemoJcApplication.java
@@ -0,0 +1,13 @@
+package com.sb.jpa.jpa_demo_jc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDemoJcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDemoJcApplication.class, args);
+	}
+
+}
---
+++ SJPA_Practice/jpa-demo-jc/src/test/java/com/sb/jpa/jpa_demo_jc/JpaDemoJcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.jpa.jpa_demo_jc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoJcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
